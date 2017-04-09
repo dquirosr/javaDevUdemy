47544e95b83e4f671b2328a644dfdaa011ddde37
Diego QR <fuckme@castor>
2017-04-09 09:56:44 -0600

    2do commit

--- Hello.java
+++ Hello.java
@@ -4,5 +4,6 @@ FIRST JAVA PROGRAM WROTE BY CLI
 public class Hello{
 	public static void main(String[] args) {
 		System.out.println("Hello M..Fuck..!!");
+		System.out.println("2do commit");
 	}
 }
